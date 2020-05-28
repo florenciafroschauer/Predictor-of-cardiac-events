2723adff3d59aba7ef34b4690b1176774cb5da05
JuanDeLorenzo <juancruzdelor@gmail.com>
2020-05-28 16:07:33 -0300

    rmd

--- query.R
+++ query.R
@@ -1,5 +1,5 @@
 #Principales factores de riesgo: Presion arterial alta(HTA), colesterol(DLP), diabetes(DBT), obesidad(OBES),
-#tabaquismo(TBQ), genero(Genero) y herencia(AHF), edad mayor a 65(Edad>65), 
+#tabaquismo(TBQ), genero(Genero) y herencia(AHF), edad mayor a 40(Edad>40), 
 
 library(plotrix)
 library(ggridges)
@@ -41,7 +41,7 @@ ggplot(pacientsQ2, aes(x = Edad, fill = Genero)) +
 #Question 3 : Frecuencia de edad de hipertencion en gente con eventos cardiacos
 pacientsQ3 = subset(pacientsWithEvents, HTA == 1)
 
-#Question 4 : Relacion genero con eventos cardiacos
+#Question 4 : Relacion genero con eventos cardiacos (DLP, DBT, OBES)
 pacientsQ4 = subset(pacientsWithEvents, select = c(Edad, Genero))
 pacientsQ4$Genero[pacientsQ4$Genero == "1"] = "Hombre"
 pacientsQ4$Genero[pacientsQ4$Genero == "0"] = "Mujer"
@@ -53,7 +53,9 @@ ggplot(pacientsQ4, aes(x = Edad, fill = Genero)) +
   theme_ridges() + 
   xlab("Age") +
   ylab("Density") +
-  ggtitle("Range of ages from patients with DBT, DLP and OBES")
+  ggtitle("Range of ages from patients with DBT, DLP and OBES") + 
+  theme_minimal()
+  
 
 #Question 5 : Cantidad de mujeres y varones con eventos (Data Study)
 patientsQ5 = subset(pacientsWithEvents, select = (Genero))
@@ -65,15 +67,16 @@ pct = round(slices/sum(slices)*100)
 lbls <- paste(lbls, pct)
 lbls <- paste(lbls,"%",sep="")
 
-pie3D(slices,labels=lbls ,main="Patients with events", col=c("#66ccff", "#ff99cc"), explode = 0.1, theta = 1, mar = c(5,5,5,5))
+pie3D(slices,labels=lbls ,main="Patients with events", col=c("#66ccff", "#ff99cc"), explode = 0.1, theta = 1, mar = c(5,5,5,5), labelcex = 1)
 
 
-#Question 8 : Cantidad pacientes con eventos >65años y <65años:
+#Question 8 : Cantidad pacientes con eventos >65 y <65:
 patientsQ8 = subset(pacientsWithEvents, select = (Edad))
-more65 <- round(sum(patientsQ8$Edad>65)/length(patientsQ8$Edad)*100)
-less65 <- round(sum(patientsQ8$Edad<=50)/length(patientsQ8$Edad)*100)
-pie3D(c(more65,less65),main = "Patients with events age´s", labels = c(paste(more65,"%"),paste(less65,"%")),col = c("lightgreen","#ffff99"),labelcex = 1, explode = 0.1, theta = 1, mar = c(5,5,5,5))
-legend(.3,1,c("Age >65","Age <=65"),fill = c("lightgreen","#ffff99"))
+more65 <- round(sum(patientsQ8$Edad>55)/length(patientsQ8$Edad)*100)
+less65 <- round(sum(patientsQ8$Edad<=55)/length(patientsQ8$Edad)*100)
+pie3D(c(more65,less65),main = "Patients percentege by age", labels = c(paste(more65,"%"),paste(less65,"%")),col = c("lightgreen","#ffff99"),labelcex = 1, start = 0, explode = 0.06, theta = 1, mar = c(5,5,5,5))
+legend(.3,1,c("Age >55","Age <=55"),fill = c("lightgreen","#ffff99"))
+
 
 #Question 6 : Rango de edades de personas con un evento cardiaco, fumadores y que presente hipertension arterial
 pacientsQ6 = subset(pacientsWithEvents, TBQ == 1 & HTA == 1)
@@ -95,5 +98,94 @@ ggplot(data, aes(x=x, y=y)) +
   ylab("Density") +
   ggtitle("Relation between DLP, DBT and OBES") + 
   theme_ridges()
-  
+
+
+#Range of ages for each risk 
+
+##HTA
+patientsHTA = subset(pacientsWithEvents, HTA == 1)
+patientsHTA = subset(patientsHTA, select = c(Edad, Genero))
+patientsHTA$Genero[patientsHTA$Genero == "1"] = "Hombre"
+patientsHTA$Genero[patientsHTA$Genero == "0"] = "Mujer"
+
+ggplot(patientsHTA, aes(x = Edad, fill = Genero)) +
+  geom_density(alpha = 0.4) +
+  scale_fill_discrete(name = "Gender", labels = c("Female","Male")) +
+  theme_ridges() + 
+  xlab("Age") +
+  ylab("Density") +
+  ggtitle("Range of ages from patients with hipertension") + 
+  theme_minimal()
+
+##DLP
+patientsDLP = subset(pacientsWithEvents, DLP == 1)
+patientsDLP = subset(patientsDLP, select = c(Edad, Genero))
+patientsDLP$Genero[patientsDLP$Genero == "1"] = "Hombre"
+patientsDLP$Genero[patientsDLP$Genero == "0"] = "Mujer"
+
+ggplot(patientsDLP, aes(x = Edad, fill = Genero)) +
+  geom_density(alpha = 0.4) +
+  scale_fill_discrete(name = "Gender", labels = c("Female","Male")) +
+  theme_ridges() + 
+  xlab("Age") +
+  ylab("Density") +
+  ggtitle("Range of ages from patients with Cholesterol") + 
+  theme_minimal()
+
+##TBQ
+patientsTBQ = subset(pacientsWithEvents, TBQ == 1)
+patientsTBQ = subset(patientsTBQ, select = c(Edad, Genero))
+patientsTBQ$Genero[patientsTBQ$Genero == "1"] = "Hombre"
+patientsTBQ$Genero[patientsTBQ$Genero == "0"] = "Mujer"
+
+ggplot(patientsTBQ, aes(x = Edad, fill = Genero)) +
+  geom_density(alpha = 0.4) +
+  scale_fill_discrete(name = "Gender", labels = c("Female","Male")) +
+  theme_ridges() + 
+  xlab("Age") +
+  ylab("Density") +
+  ggtitle("Range of ages from smoking patients") + 
+  theme_minimal()
+
+##OBES
+patientsOBES = subset(pacientsWithEvents, OBES == 1)
+patientsOBES = subset(patientsOBES, select = c(Edad, Genero))
+patientsOBES$Genero[patientsOBES$Genero == "1"] = "Hombre"
+patientsOBES$Genero[patientsOBES$Genero == "0"] = "Mujer"
+
+ggplot(patientsOBES, aes(x = Edad, fill = Genero)) +
+  geom_density(alpha = 0.4) +
+  scale_fill_discrete(name = "Gender", labels = c("Female","Male")) +
+  theme_ridges() + 
+  xlab("Age") +
+  ylab("Density") +
+  ggtitle("Range of ages from patientes with obesity") + 
+  theme_minimal()
+
+##DBT
+
+patientsDBT = subset(pacientsWithEvents, DBT == 1)
+patientsDBT= subset(patientsDBT, select = c(Edad, Genero))
+patientsDBT$Genero[patientsDBT$Genero == "1"] = "Hombre"
+patientsDBT$Genero[patientsDBT$Genero == "0"] = "Mujer"
+
+ggplot(patientsDBT, aes(x = Edad, fill = Genero)) +
+  geom_density(alpha = 0.4) +
+  scale_fill_discrete(name = "Gender", labels = c("Female","Male")) +
+  theme_ridges() + 
+  xlab("Age") +
+  ylab("Density") +
+  ggtitle("Range of ages from patients with diabetes") + 
+  theme_minimal()
+
+## DBT, HTA , OBES
+
+
+Risk factor   | Patients with events | Patients without events
+------------- | -------------------- | -----------------------
+Hypertension  | 62%                  | 29%
+Cholesterol   | 58%                  | 24%         
+Smoking       | 46%                  | 23%
+Obesity       | 45%                  | 19%
+Diabetes      | 27%                  | 9%  
 
